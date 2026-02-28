f3e5a2e98e63f62a7f6d5c0dc5e6a462ba2694e3
Joshua <st10296167@vcconnect.edu.za>
2026-03-01 01:46:45 +0200

    Fix redux store

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -14,7 +14,7 @@ type Props = {
 };
 
 const Providers = ({ children }: Props) => {
-  const { store, persistor } = makeStore();
+  const [{ store, persistor }] = React.useState(() => makeStore());
 
   return (
     <Provider store={store}>
